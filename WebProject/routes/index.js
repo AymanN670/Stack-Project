28d654ecea2b1f12070fdc8d97e453c8d334ca9e
AymanN670 <ayman.najmuddin@ontariotechu.net>
2024-11-05 15:39:45 -0500

    removed services and products and kept the ones i needed

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -1,34 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Home'});
+  res.render('index', { title: 'Home' });
 });
 
-/* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', {title: 'Home'});
+  res.render('index', { title: 'Home' });
 });
 
-/* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', {title: 'About'});
+  res.render('index', { title: 'About' });
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', {title: 'Products'});
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
 });
 
-/* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', {title: 'Services'});
-});
-
-/* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', {title: 'Contact us'});
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
